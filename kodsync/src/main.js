2c9a3b930c40d9d2a26f415da6dc75d801bc5eb0
HamitEldem <meydanlar-guveler0l@icloud.com>
2025-02-15 03:19:53 +0300

    updates

--- kodsync/src/main.js
+++ kodsync/src/main.js
@@ -3,6 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+console.log('App is initializing...')
+
 createApp(App)
   .use(router)
   .mount('#app')
